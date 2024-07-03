82b43c168aa3f804921013ed846700d9a7afa5ad
linelie00 <dingpawn@gmail.com>
2024-07-03 23:34:10 +0900

    [Feat]:기본적인 틀 작성

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config(); // .env 파일의 환경 변수 로드
+
+const app = express();
+app.set('port', process.env.PORT || 8282);
+
+// CORS 설정
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
+// 서버 실행
+app.listen(app.get('port'), () => {
+  console.log(`${app.get('port')}번 포트에서 서버가 실행 중입니다.`);
+});
---
+++ server/src/controllers/authController.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config(); // .env 파일의 환경 변수 로드
+
+Router.get('/login', (req, res) => {
+   
+});
+
+module.exports = Router;
