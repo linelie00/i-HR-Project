ebea73e053f951b22e1718d35af801a5e94e37f0
linelie00 <dingpawn@gmail.com>
2024-08-14 17:13:21 +0900

    [Feat]: 모듈 다운로드 및 설정

--- server/server.js
+++ server/server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const { sequelize } = require('./models');
+const authRoutes = require('./src/routes/authRoute.js');
 
 dotenv.config(); // .env 파일의 환경 변수 로드
 
 const app = express();
 app.set('port', process.env.PORT || 8080);
 
+// 데이터베이스 연결
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error('데이터베이스 연결 실패:', err);
+  });
+
 // CORS 설정
 app.use(cors({
   origin: process.env.CLIENT_URL, // .env 파일에서 가져온 CLIENT_URL 사용
   credentials: true
 }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//라우터 설정
+app.use('/api', authRoutes);
 
 // 서버 실행
 app.listen(app.get('port'), () => {
--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware'); 
+const authMiddleware = require('../middlewares/authMiddleware'); 
 
 const router = express.Router();
 
